0f324a2091e5bbf990eea0a61d6ad45662f10120
Linwe-e <199511961+Linwe-e@users.noreply.github.com>
2025-05-15 17:48:28 +0200

    app.py fonctionne

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from pycaret.regression import load_model, predict_model
 # Loading the trained model
 # Try except verification
 try:
-    loaded_model = load_model('Light Gradient Boosting Machine')
+    loaded_model = load_model('pipeline_immo_eliza')
     # st.sidebar.success("Modèle PyCaret chargé avec succès !")
 except Exception as e:
     st.error(f"Erreur lors du chargement du modèle PyCaret : {e}")
@@ -15,12 +15,13 @@ except Exception as e:
 
 
 EXPECTED_COLUMNS_ORDER = [
-    'type', 'subtype', 'bedroomCount', 'bathroomCount', 'locality',
-    'postCode', 'habitableSurface', 'buildingCondition',
-    'buildingConstructionYear', 'facedeCount', 'floodZoneType',
-    'heatingType', 'kitchenType', 'hasGarden_1', 'toiletCount',
-    'hasSwimmingPool_True', 'hasFireplace_True', 'hasTerrace_True',
-    'epcScore']
+    'type', 'bedroomCount', 'bathroomCount', 'province', 'locality',
+       'postCode', 'habitableSurface', 'buildingCondition',
+       'buildingConstructionYear', 'facedeCount', 'floodZoneType',
+       'heatingType', 'kitchenType', 'landSurface', 'hasGarden',
+       'gardenSurface', 'toiletCount', 'hasSwimmingPool', 'hasFireplace',
+       'hasTerrace', 'subtype_grouped', 'building_floors',
+       'apartment_floor', 'region', 'epcNumeric']
 
 
 
@@ -42,9 +43,76 @@ def immo_prediction(list_input_data):
     
     # Changin the data into a Dataframe
     df_input_data = pd.DataFrame([list_input_data], columns=EXPECTED_COLUMNS_ORDER)
+    # add region information
+    def get_region(zip_code):
+        # On tente de convertir en int
+        try:
+            z = int(zip_code)
+        except (ValueError, TypeError):
+            return pd.NA    
+
+        if 1000 <= z <= 1299:
+            return "Bruxelles"
+        elif 1300 <= z <= 1499 or 4000 <= z <= 7999:
+            return "Wallonia"
+        else:
+            return "Flanders"
+
+    df_input_data['region'] = df_input_data['postCode'].apply(get_region)
+
+    def epcToNumeric(row):
+        region = row['region']
+        epc_score = row['epcNumeric']
+        
+        epc_mapping = {
+            'Flanders': {
+                'A++': 0,
+                'A+': 0,
+                'A': 100,
+                'B': 200,
+                'C': 300,
+                'D': 400,
+                'E': 500,
+                'F': 600,
+                'G': 700
+            },
+            'Wallonia': {
+                'A++': 0,
+                'A+': 50,
+                'A': 90,
+                'B': 170,
+                'C': 250,
+                'D': 330,
+                'E': 420,
+                'F': 510,
+                'G': 600
+            },
+            'Bruxelles': {
+                'A++': 0,
+                'A+': 0,
+                'A': 45,
+                'B': 95,
+                'C': 145,
+                'D': 210,
+                'E': 275,
+                'F': 345,
+                'G': 450
+            }
+        }
+        
+        return epc_mapping.get(region, {}).get(epc_score, None)
+    
+    df_input_data['epcNumeric'] = df_input_data.apply(epcToNumeric, axis=1)
 
     st.subheader("Data send to the predict model :") # For debug
     # st.dataframe(df_input_data)
+
+    print("Colonnes de df_input_data:", df_input_data.columns.tolist())
+    print("Nombre de colonnes de df_input_data:", len(df_input_data.columns))
+    print("Première ligne de df_input_data:\n", df_input_data.head())
+    # Si possible, affiche aussi les types de données pour vérifier
+    print("Types de données de df_input_data:\n", df_input_data.dtypes)
+
 
     try:
         # Utiliser predict_model de PyCaret
@@ -93,24 +161,27 @@ def main():
     st.sidebar.header('Enter property details :')
 
     # Numerical data
-    bedroomCount = st.sidebar.number_input('Bedroom Count', min_value=0, value=2, step=1)
-    bathroomCount = st.sidebar.number_input('Bathroom Count', min_value=0, value=1, step=1)
+    bedroomCount = st.sidebar.slider('Bedroom Count', min_value=0, max_value=10, value=2, step=1)
+    bathroomCount = st.sidebar.slider('Bathroom Count', min_value=0, max_value=10, value=2, step=1)
     postCode = st.sidebar.number_input('Post Code', min_value=1000, max_value=9999, value=1000, step=1)
-    habitableSurface = st.sidebar.slider('Habitable Surface',min_value=0, max_value=1000, value=100, step=1)
-    buildingConstructionYear = st.sidebar.slider('Building Construction Year', min_value=1800, max_value=2025, value=1990, step=1)
+    habitableSurface = st.sidebar.number_input('Habitable Surface',min_value=0, value=100, step=1)
+    buildingConstructionYear = st.sidebar.number_input('Building Construction Year', min_value=1800, max_value=2025, value=1990, step=1)
     facedeCount = st.sidebar.number_input('Facede Count', min_value=0, value=2, step=1)
     toiletCount = st.sidebar.number_input('Toilet Count', min_value=0, value=1, step=1)
+    landSurface = st.sidebar.number_input('Land Surface', min_value=0, value=100, step=1)
+    gardenSurface = st.sidebar.number_input('Garden Surface', min_value=0, value=100, step=1)
+    building_floors = st.sidebar.number_input('Building Floors for a house', min_value=0, value=2, step=1)
+    apartment_floor = st.sidebar.number_input('Apartment Floor for an apartment', min_value=0, value=2, step=1)
 
     # Categorical data
     type = st.sidebar.selectbox('Type', ['House', 'Appartment']) 
 
-    subtype = st.sidebar.selectbox('SubType', [
-        'EXCEPTIONAL_PROPERTY', 'VILLA', 'HOUSE', 'MANSION', 'MIXED_USE_BUILDING', 
-        'PENTHOUSE', 'APARTMENT', 'DUPLEX', 'GROUND_FLOOR', 'CASTLE',
-        'COUNTRY_COTTAGE', 'MANOR_HOUSE', 'FARMHOUSE', 'TOWN_HOUSE', 'LOFT', 'TRIPLEX',
-        'OTHER_PROPERTY', 'BUNGALOW', 'KOT', 'SERVICE_FLAT', 'FLAT_STUDIO', 'CHALET',
-        'PAVILION'
-    ])
+    subtype_grouped = st.sidebar.selectbox('SubType', ['STANDARD_HOUSE', 'STANDARD_APARTMENT',
+                                                       'LUXURY_PROPERTY', 'SPECIAL_APARTMENT',
+                                                       'MIXED_USE', 'RURAL_HOUSE', 'OTHER'])
+
+
+
 
     buildingCondition = st.sidebar.selectbox('Building Condition', [
         'GOOD', 'AS_NEW', 'JUST_RENOVATED', 
@@ -135,30 +206,47 @@ def main():
         'USA_SEMI_EQUIPPED'
     ])
 
-    epcScore = st.sidebar.selectbox('EPC Score', [
+    epcNumeric = st.sidebar.selectbox('EPC Score', [
         'B', 'A', 'C', 'F', 'D', 'E', 'G', 'A+', 'A++'
     ])
 
     locality = st.sidebar.text_input('Locality') # What happen if the name is wrong spelled ?
 
+    province = st.sidebar.selectbox('Province', ['West Flanders',      
+                                                'Antwerp',           
+                                                'East Flanders',     
+                                                'Flemish Brabant',   
+                                                'Hainaut',           
+                                                'Liège',            
+                                                'Limburg',           
+                                                'Luxembourg',      
+                                                'Namur',             
+                                                'Walloon Brabant',
+                                                'Brussels'])
+    
+    region = st.sidebar.selectbox('Region',['Bruxelles', 'Wallonia', 'Flanders'])
+
+      
+
     # Bolean data
     st.sidebar.write('Additionnal features')
-    hasGarden_1 = st.sidebar.checkbox('Garden', value=False)
-    hasSwimmingPool_True = st.sidebar.checkbox('Swimming Pool', value=False)
-    hasFireplace_True = st.sidebar.checkbox('Fireplace', value=False)
-    hasTerrace_True = st.sidebar.checkbox('Terrace', value=False)
+    hasGarden = st.sidebar.checkbox('Garden', value=False)
+    hasSwimmingPool = st.sidebar.checkbox('Swimming Pool', value=False)
+    hasFireplace = st.sidebar.checkbox('Fireplace', value=False)
+    hasTerrace = st.sidebar.checkbox('Terrace', value=False)
 
     # Code for prediction
     price_prediction = ''
     
     # Creating a button for prediction
     if st.button('Predict Price'):
-        list_input_data = [type, subtype, bedroomCount, bathroomCount, locality,
-        postCode, habitableSurface, buildingCondition,
-        buildingConstructionYear, facedeCount, floodZoneType,
-        heatingType, kitchenType, hasGarden_1, toiletCount,
-        hasSwimmingPool_True, hasFireplace_True, hasTerrace_True,
-        epcScore]
+        list_input_data = [type, bedroomCount, bathroomCount, province, locality,
+       postCode, habitableSurface, buildingCondition,
+       buildingConstructionYear, facedeCount, floodZoneType,
+       heatingType, kitchenType, landSurface, hasGarden,
+       gardenSurface, toiletCount, hasSwimmingPool, hasFireplace,
+       hasTerrace, subtype_grouped, building_floors,
+       apartment_floor, region, epcNumeric]
 
         predicted_price_value = immo_prediction(list_input_data)
 
